4f91d72b0300aee8802fcad22433cfd68bad789c
ChiuDave <e2243115@cmaisonneuve.qc.ca>
2024-10-17 14:00:57 -0400

    modified register and added dashboard

--- src/composants/Home.jsx
+++ src/composants/Home.jsx
@@ -1,27 +1,30 @@
 import { useNavigate } from "react-router-dom";
-
-
+import "./Home.css";
 
 function Home() {
-
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
     const handleBtnConnexion = () => {
-        navigate("/connexion")
-    }
+        navigate("/connexion");
+    };
 
     const handleBtnInscription = () => {
-        navigate("/register")
-    }
-    return(
-        <div>
-            <label> <h1>TP1 site web transactionel et chat </h1></label>
-            <div>
-                <button onClick={handleBtnConnexion}> Connexion </button>
-                <br />
-                <button onClick={handleBtnInscription}> Incription </button>
+        navigate("/register");
+    };
+
+    return (
+        <div className="home-container">
+            <h1 className="home-title">TP1 Site Web Transactionnel et Chat</h1>
+            <div className="button-container">
+                <button className="home-btn" onClick={handleBtnConnexion}>
+                    Connexion
+                </button>
+                <button className="home-btn" onClick={handleBtnInscription}>
+                    Inscription
+                </button>
             </div>
         </div>
-    ) 
+    );
 }
+
 export default Home;
